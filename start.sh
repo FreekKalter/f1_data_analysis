b6d5438141cebf929760ebb3fec59d5a3df7d207
FreekKalter <freek@kalteronline.org>
2019-11-09 13:46:27 +0100

    update csv files before starting

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 if [[ -d ./venv ]]; then
     source ./venv/bin/activate
+    python ./prepcsv/lib/prepcsv.py
     jupyter notebook --no-browser
 else
     python3 -m venv "$(pwd)/venv"
     source ./venv/bin/activate
     python -m pip install --upgrade pip
     python -m pip install -r ./requirements.txt
+    python ./prepcsv/lib/prepcsv.py
     jupyter notebook --no-browser
 fi
