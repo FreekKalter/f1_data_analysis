5207d5b822d0e0159153d7f8c460b7860f01f6a5
FreekKalter <freek@kalteronline.org>
2019-11-09 13:02:27 +0100

    combined init and start script

--- init.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-python3 -m venv "$(pwd)/venv"
-source ./venv/bin/activate
-pip install --upgrade pip
-pip install -r ./requirements.txt
--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-source ./venv/bin/activate
-jupyter notebook --no-browser
+if [[ -d ./venv ]]; then
+    source ./venv/bin/activate
+    jupyter notebook --no-browser
+else
+    python3 -m venv "$(pwd)/venv"
+    source ./venv/bin/activate
+    pip -m pip install --upgrade pip
+    pip -m pip install -r ./requirements.txt
+    jupyter notebook --no-browser
+fi
